ecf02881ece6013901f0965188583fb043d3c78f
Citrous17 <raceboy1290@live.com>
2022-04-27 09:37:09 -0500

    Add framework for Newton

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Netwon
 A discord bot that can help you with your math homework!
 
-Newton employs Autocode in order to handle the server requests that a user makes to the bot.
+Newton employs discord.py in order to handle the server requests that a user makes to the bot.
---
+++ src/bot.py
@@ -0,0 +1,34 @@
+# bot.py
+import os
+import math
+from cv2 import add
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+
+bot = commands.Bot(command_prefix='!Newton ')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name="add")
+async def addition(ctx, num1: float, num2: float):
+    response = num1 + num2
+    await ctx.send(response)
+
+@bot.command(name="Compute")
+async def computation(ctx, problem):
+    ans = 0
+    if("+" in problem):               
+        problemArray = problem.split('+')
+        for i, v in enumerate(problemArray):
+            problemArray[i] = float(v)
+        ans += math.fsum(problemArray)
+    response = "```\n" + str(problem) + "\n" + str(ans) + "```"
+    await ctx.send(response)
+
+bot.run(TOKEN)
