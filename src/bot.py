b3e65d655c18e84483af6dd2847136f84a8b6595
Citrous17 <raceboy1290@live.com>
2022-04-28 14:37:09 -0500

    Add processCommand

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,14 @@
 # bot.py
 import os
 import math
+import xml.etree.ElementTree as ET
+import requests
 from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+WOLFRAM_TOKEN = os.getenv('WOLFRAM_TOKEN')
 
 bot = commands.Bot(command_prefix='!Newton ')
 
@@ -15,18 +18,22 @@ async def on_ready():
 
 @bot.command(name="Compute")
 async def computation(ctx, problem):
-    ans = 0
-    if("+" in problem):               
-        problemArray = problem.split('+')
-        for i, v in enumerate(problemArray):
-            problemArray[i] = float(v)
-        ans += math.fsum(problemArray)
-    response = "```\n" + str(problem) + "\n" + str(ans) + "```"
+    query = problem[problem.find('(')+1:problem.find(')')]
+    await ctx.send(query)
+    wolframResponse = processCommand(query, problem)
+    await ctx.send(problem)
+    response = str(wolframResponse)
     await ctx.send(response)
-
+    
+def processCommand(query, command):
+    if "ask Wolfram" in command:
+        request = "http://api.wolframalpha.com/v2/query?input=" + str(query) + "&appid=" + str(WOLFRAM_TOKEN) + "&format=image"
+        return str(request)
+    return "error"
+    
 @bot.command(name="info")
 async def information(ctx):
     response = "I am a bot that specializes in all problems for Calculus 1 and Calculus 2!"
     await ctx.send(response)
 
-bot.run(TOKEN)
+bot.run(DISCORD_TOKEN)
